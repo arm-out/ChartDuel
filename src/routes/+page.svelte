090ece78f8c4374f0072b3c4613b316b60425b3c
arm-out <arminsuraj@gmail.com>
2023-12-10 21:30:55 -0800

    basic game logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,55 @@
 <script lang="ts">
+	import type { ComponentEvents } from 'svelte';
 	import Card from './Card.svelte';
+	import { nextSong } from '$lib/client';
+	import type { Tables } from '$lib/types/supabase';
 	export let data;
 
 	let base = data.base;
 	let cur = data.cur;
 	let next = data.next;
+
+	$: preloadUrl = next.image;
+
+	// Game logic
+	let score = 0;
+	let highScore = 0;
+
+	async function handleGuess(event: ComponentEvents<Card>['guess']) {
+		if (event.detail.guess === 'higher') {
+			if (cur.streams > base.streams) {
+				score++;
+			} else {
+				highScore = Math.max(score, highScore);
+				score = 0;
+			}
+		} else {
+			if (cur.streams < base.streams) {
+				score++;
+			} else {
+				highScore = Math.max(score, highScore);
+				score = 0;
+			}
+		}
+
+		base = cur;
+		cur = next;
+		next = (await nextSong(1))[0];
+	}
 </script>
+
+<svelte:head>
+	<link rel="preload" as="image" href={preloadUrl} />
+</svelte:head>
 
 <div class="grid grid-cols-2 gap-1 bg-spotify-green">
 	<Card {...base} />
-	<Card {...cur} hidden={true} />
+	<Card on:guess={handleGuess} {...cur} guess={true} />
 </div>
+
+<p class="bg-spotify-dark text-white absolute bottom-0 pl-5 text-3xl pb-5">
+	High Score: {highScore}
+</p>
+<p class="bg-spotify-dark text-white absolute bottom-0 right-0 pr-5 text-3xl pb-5">
+	Score: {score}
+</p>
--- src/routes/Card.svelte
+++ src/routes/Card.svelte
@@ -1,27 +1,61 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
 	export let title: string | null;
 	export let artist: string | null;
 	export let image: string | null;
-	export let streams: number | null;
-	export let hidden: boolean = false;
+	export let streams: number;
+	export let guess: boolean = false;
+
+	const dispatch = createEventDispatcher();
+
+	let hasGuessed = false;
+
+	let answerCounter = 0;
+
+	function animateStreams() {
+		while (answerCounter < streams) {
+			answerCounter += 1;
+		}
+	}
+
+	function handleGuess(g: 'higher' | 'lower') {
+		// setTimeout(() => {}, 5000);
+		hasGuessed = true;
+		// animateStreams();
+		setTimeout(() => {
+			dispatch('guess', { guess: g });
+		}, 1000);
+		hasGuessed = false;
+		answerCounter = 0;
+	}
 </script>
 
-<div class="bg-spotify-dark h-screen flex flex-col items-center pt-44">
-	<img src={image} class="h-80" alt={title} />
-	<p class="text-white w-80 pt-5 text-3xl">{title}</p>
-	<p class="text-spotify-light w-80 pt-2 text-xl pb-10">{artist}</p>
-	{#if !hidden}
-		<p class="text-spotify-green text-6xl font-bold">{streams?.toLocaleString()}</p>
-		<p class="text-spotify-light pt-1">Total global streams</p>
-	{/if}
-	{#if hidden}
-		<button
-			class="text-white text-xl p-4 px-12 bg-spotify-dark border-2 border-solid border-spotify-green rounded-full mb-4"
-			>Higher</button
-		>
-		<button
-			class="text-white text-xl p-4 px-12 bg-spotify-dark border-2 border-solid border-red-700 rounded-full bg-"
-			>Lower</button
-		>
-	{/if}
+<div class="bg-spotify-dark h-screen flex flex-col flex-initial items-center pt-[20vh] space-y-12">
+	<div>
+		<img src={image} class="h-80" alt={title} />
+		<p class="text-white w-80 pt-5 text-3xl">{title}</p>
+		<p class="text-spotify-light w-80 pt-2 text-xl">{artist}</p>
+	</div>
+
+	<div class="flex flex-col items-center">
+		{#if !guess}
+			<p class="text-spotify-green text-6xl font-bold">{streams?.toLocaleString()}</p>
+			<p class="text-spotify-light pt-1">Total global streams</p>
+		{:else if !hasGuessed}
+			<button
+				on:click={() => handleGuess('higher')}
+				class="text-white text-xl p-4 px-12 bg-spotify-dark border-2 border-solid border-spotify-green rounded-full mb-3"
+				>Higher</button
+			>
+			<button
+				on:click={() => handleGuess('lower')}
+				class="text-white text-xl p-4 px-12 bg-spotify-dark border-2 border-solid border-red-700 rounded-full"
+				>Lower</button
+			>
+		{:else}
+			<p class="text-white text-6xl font-bold">{answerCounter.toLocaleString()}</p>
+			<p class="text-spotify-light pt-1">Total global streams</p>
+		{/if}
+	</div>
 </div>
