c7b22575be8537507df0b8deacba159768f4dcaa
arm-out <arminsuraj@gmail.com>
2023-12-11 21:37:14 -0800

    fixed timesPlayed store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -7,6 +7,8 @@ export const highScore = writable(
 highScore.subscribe((value) => browser && localStorage.setItem('highScore', String(value)));
 
 export const timesPlayed = writable(
-	browser && localStorage.getItem('highScore') ? parseInt(localStorage.getItem('highScore')!) : 0
+	browser && localStorage.getItem('timesPlayed')
+		? parseInt(localStorage.getItem('timesPlayed')!)
+		: 0
 );
 timesPlayed.subscribe((value) => browser && localStorage.setItem('timesPlayed', String(value)));
