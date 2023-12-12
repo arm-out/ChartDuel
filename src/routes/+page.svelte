5488969474884bb309b6cf4b7f23e88fd191afc5
arm-out <arminsuraj@gmail.com>
2023-12-11 16:48:53 -0800

    animations and game loop logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import EndModal from './EndModal.svelte';
 	import type { ComponentEvents } from 'svelte';
 	import Card from './Card.svelte';
 	import { nextSong } from '$lib/client';
@@ -9,17 +10,34 @@
 	let cur = data.cur;
 	let next = data.next;
 
+	$: console.log(base.title + ', ' + cur.title + ', ' + next.title);
+
+	let animate = false;
+	let rerender = false; // trigger
+
 	$: preloadUrl = next.image;
 
 	// Game logic
 	let score = 0;
 	let highScore = 0;
+	let prevScore = score;
+	let endScreen = false;
 
 	async function getColor(song: Song) {
 		const imgUrl = song.image.split('/');
 		const response = await fetch('/api/color?img=' + imgUrl[imgUrl.length - 1]);
 		const color = await response.json();
+		console.log('got color');
 		song.color = color.color;
+
+		rerender = !rerender;
+	}
+
+	function endGame() {
+		highScore = Math.max(score, highScore);
+		prevScore = score;
+		score = 0;
+		endScreen = true;
 	}
 
 	async function handleGuess(event: ComponentEvents<Card>['guess']) {
@@ -27,22 +45,24 @@
 			if (cur.streams > base.streams) {
 				score++;
 			} else {
-				highScore = Math.max(score, highScore);
-				score = 0;
+				endGame();
 			}
 		} else {
 			if (cur.streams < base.streams) {
 				score++;
 			} else {
-				highScore = Math.max(score, highScore);
-				score = 0;
+				endGame();
 			}
 		}
 
-		base = cur;
-		cur = next;
-		next = (await nextSong(1))[0];
-		await getColor(next);
+		animate = true;
+		setTimeout(async () => {
+			animate = false;
+			base = cur;
+			cur = next;
+			next = (await nextSong(1))[0];
+			await getColor(next);
+		}, 1000);
 	}
 </script>
 
@@ -50,14 +70,58 @@
 	<link rel="preload" as="image" href={preloadUrl} />
 </svelte:head>
 
-<div class="grid grid-cols-2 gap-1 bg-spotify-green">
-	<Card {...base} color={base.color} />
-	<Card on:guess={handleGuess} {...cur} color={cur.color} guess={true} />
+<div class=" bg-spotify-green h-screen w-screen overflow-hidden absolute">
+	{#key base.id}
+		<section
+			class="{animate
+				? 'carousel-animation'
+				: ''} h-full w-[calc(50%-0.125rem)] absolute left-0 bg-spotify-dark"
+		>
+			<Card {...base} color={base.color} />
+		</section>
+		<section
+			class="{animate
+				? 'carousel-animation'
+				: ''} h-full w-[calc(50%-0.125rem)] absolute right-0 bg-spotify-dark"
+		>
+			<Card on:guess={handleGuess} {...cur} color={cur.color} guess={true} answer={base.streams} />
+		</section>
+	{/key}
+	{#key rerender}
+		<section
+			class="{animate
+				? 'carousel-animation'
+				: ''} h-full w-[calc(50%-0.125rem)] absolute left-[calc(100%+0.26rem)] bg-spotify-dark"
+		>
+			<Card on:guess={handleGuess} {...next} color={next.color} guess={true} />
+		</section>
+	{/key}
 </div>
 
-<p class=" text-white absolute bottom-0 pl-5 text-3xl pb-5">
+<p class="text-white absolute bottom-0 pl-5 text-3xl pb-5">
 	High Score: {highScore}
 </p>
 <p class=" text-white absolute bottom-0 right-0 pr-5 text-3xl pb-5">
 	Score: {score}
 </p>
+
+<EndModal bind:endScreen>
+	<h2 slot="header">{prevScore}</h2>
+	<p>High Score: {highScore}</p>
+	<p>Nice try</p>
+</EndModal>
+
+<style>
+	@keyframes moveLeft {
+		0% {
+			transform: translateX(0);
+		}
+		100% {
+			transform: translateX(calc(-100% - 0.25rem));
+		}
+	}
+
+	.carousel-animation {
+		animation: moveLeft 1s ease-in-out;
+	}
+</style>
--- src/routes/Card.svelte
+++ src/routes/Card.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
+	import { createEventDispatcher, onMount } from 'svelte';
 
 	export let title: string | null;
 	export let artist: string | null;
@@ -7,20 +7,51 @@
 	export let streams: number;
 	export let color: string | undefined;
 	export let guess: boolean = false;
+	export let answer: number = 0;
 
 	const dispatch = createEventDispatcher();
 
 	let hasGuessed = false;
+	let countDisplay = 0;
+	let speed = 200;
+	let textColor = 'text-white';
+	let guessBtn: string = '';
+
+	function animate() {
+		const time = streams / speed;
+		if (countDisplay < streams) {
+			countDisplay = Math.ceil(countDisplay + time);
+			setTimeout(animate, 1);
+		} else {
+			countDisplay = streams;
+			setTimeout(() => {
+				console.log(guessBtn, streams.toLocaleString(), answer.toLocaleString());
+				if (
+					(guessBtn === 'higher' && streams > answer) ||
+					(guessBtn === 'lower' && streams < answer)
+				) {
+					textColor = 'text-spotify-green';
+				} else {
+					textColor = 'text-red-700';
+				}
+			}, 250);
+		}
+	}
 
 	function handleGuess(g: 'higher' | 'lower') {
 		hasGuessed = true;
-		dispatch('guess', { guess: g });
-		hasGuessed = false;
+		console.log('guessing', g);
+		guessBtn = g;
+		animate();
+
+		setTimeout(() => {
+			dispatch('guess', { guess: g });
+		}, 1750);
 	}
 </script>
 
 <div
-	class="card-container h-screen flex flex-col flex-initial items-center pt-[20vh] space-y-12"
+	class="card-container h-screen flex flex-col flex-initial items-center pt-[20vh] space-y-12 transition-all duration-500"
 	style="background-image: linear-gradient({color}, #121212)"
 >
 	<div>
@@ -45,7 +76,9 @@
 				>Lower</button
 			>
 		{:else}
-			<p class="text-white text-6xl font-bold">{streams.toLocaleString()}</p>
+			<p class="{textColor} text-6xl font-bold transition-colors duration-200">
+				{countDisplay.toLocaleString()}
+			</p>
 			<p class="text-spotify-light pt-1">Total global streams</p>
 		{/if}
 	</div>
